c79b4c91c16e482e70b4dc5e18ab337f9a4cf503
Dodo <kasper199914@gmail.com>
2020-04-14 12:41:15 +0200

    Add logger

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,15 +1,20 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(feature = "strict", deny(warnings))]
 
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
+use env_logger::Env;
 
 const NOT_FOUND: &str =
     "{\"status\": \"Not Found\", \"Message\": \"Page not found\"}";
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .service(web::scope("/api").service(journali_api::hello))
             .default_service(web::to(|| {
                 HttpResponse::NotFound().body(NOT_FOUND)
