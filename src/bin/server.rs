f1405ffe61a8249ba1b1d6997b06fe65c477c860
Dodo <kasper199914@gmail.com>
2020-04-15 12:58:42 +0200

    add db

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,6 +2,10 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
 use actix_web::{middleware::Logger, web, App, HttpResponse, HttpServer};
+use diesel::{
+    pg,
+    r2d2::{self, ConnectionManager},
+};
 use env_logger::Env;
 
 const NOT_FOUND: &str =
@@ -11,8 +15,17 @@ const NOT_FOUND: &str =
 async fn main() -> std::io::Result<()> {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
 
-    HttpServer::new(|| {
+    dotenv::dotenv().ok();
+
+    // set up database connection pool
+    let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<pg::PgConnection>::new(connspec);
+    let pool =
+        r2d2::Pool::builder().build(manager).expect("Failed to create pool.");
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .service(web::scope("/api").service(journali_api::hello))
