c973a6430cba7a115a8cd2d9f775ed49b8e42590
Dodo <kasper199914@gmail.com>
2020-04-21 21:52:59 +0200

    skip the main fn from codecov

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -12,6 +12,7 @@ const NOT_FOUND: &str =
     "{\"status\": \"Not Found\", \"Message\": \"Page not found\"}";
 
 #[actix_rt::main]
+#[cfg_attr(tarpaulin, skip)]
 async fn main() -> std::io::Result<()> {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
 
