b2d4140ac18c4927dfc5fdfc496e65bc95cccd2b
Dodo <kasper199914@gmail.com>
2020-04-21 21:43:31 +0200

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,21 @@ use actix_web::{get, web, HttpResponse, Responder};
 pub async fn hello(data: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().body(format!("Hello sailor {}!", data.into_inner()))
 }
+
+#[actix_rt::test]
+async fn test_hello() {
+    use actix_web::{body::Body, http::StatusCode, test, web::Bytes, App};
+
+    let mut app = test::init_service(App::new().service(hello)).await;
+
+    let req = test::TestRequest::with_uri("/hello/tester").to_request();
+
+    // Call application
+    let mut resp = test::call_service(&mut app, req).await;
+    assert_eq!(resp.status(), StatusCode::OK);
+
+    assert_eq!(
+        resp.take_body().as_ref(),
+        Some(&Body::Bytes(Bytes::from("Hello sailor tester!")))
+    );
+}
