7d21c5d12642104908a0e7fe6bf803ddddf62a50
Wesley Klop <wesley19097@gmail.com>
2020-05-13 16:54:41 +0200

    Code formatting

--- src/schema.rs
+++ src/schema.rs
@@ -34,9 +34,4 @@ table! {
     }
 }
 
-allow_tables_to_appear_in_same_query!(
-    items,
-    pages,
-    todo_items,
-    todos,
-);
+allow_tables_to_appear_in_same_query!(items, pages, todo_items, todos,);
