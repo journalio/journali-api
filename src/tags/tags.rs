18906ed086a0945cab747c035b6fc7ab955996d2
DutchGhost <kasper199914@gmail.com>
2020-06-03 17:12:05 +0200

    add (unchecked) create for tags

--- src/tags/tags.rs
+++ src/tags/tags.rs
@@ -1,6 +1,10 @@
 use crate::schema::tags;
+use crate::users::user::User;
+use diesel::pg::PgConnection;
+use diesel::QueryResult;
+use diesel::RunQueryDsl;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-use serde::Serialize;
 
 #[derive(Queryable, Serialize, Insertable)]
 pub struct Tag {
@@ -8,4 +12,48 @@ pub struct Tag {
     pub name: String,
     pub color: String,
     pub owner_id: Uuid,
+}
+
+impl Tag {
+    fn from_partial(new_tag: NewTag, user: User) -> Self {
+        let NewTag { name, color } = new_tag;
+        Self { id: Uuid::new_v4(), name, color, owner_id: user.id }
+    }
+
+    fn create(
+        new_tag: NewTag,
+        user: User,
+        conn: &PgConnection,
+    ) -> QueryResult<Self> {
+        let tag = Self::from_partial(new_tag, user);
+
+        diesel::insert_into(tags::table).values(&tag).get_result(conn)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct NewTag {
+    name: String,
+    color: String,
+}
+
+mod routes {
+    use actix_web::{post, web, Error, HttpRequest, HttpResponse};
+
+    use super::{NewTag, Tag};
+    use crate::database::exec_on_pool;
+    use crate::utils::responsable::Responsable;
+    use crate::DbPool;
+    #[post("/tags")]
+    pub async fn create_tag(
+        pool: web::Data<DbPool>,
+        req: HttpRequest,
+        form: web::Json<NewTag>,
+    ) -> Result<HttpResponse, Error> {
+        let user = req.extensions().get().cloned().unwrap();
+
+        exec_on_pool(&pool, |conn| Tag::create(form.into_inner(), user, conn))
+            .await
+            .into_response()
+    }
 }
--- src/tags/tags_items.rs
+++ src/tags/tags_items.rs
@@ -1,10 +1,18 @@
 use crate::schema::tags_items;
-use uuid::Uuid;
+use diesel::prelude::*;
+use diesel::QueryResult;
 use serde::Serialize;
+use uuid::Uuid;
 
 #[derive(Queryable, Serialize, Insertable)]
 pub struct TagsItem {
     pub tag_id: Uuid,
     pub item_id: Uuid,
     pub item_type: i16,
+}
+
+impl TagsItem {
+    pub(super) fn create(&self, conn: &PgConnection) -> QueryResult<Self> {
+        diesel::insert_into(tags_items::table).values(self).get_result(conn)
+    }
 }
