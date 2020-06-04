f26e9d6fd3dbc865232d19521627d19305439930
Dodo <kasper199914@gmail.com>
2020-06-04 12:00:32 +0200

    update tag, ideas on how to get a tag

--- src/tags/tags.rs
+++ src/tags/tags.rs
@@ -5,6 +5,7 @@ use diesel::QueryResult;
 use diesel::RunQueryDsl;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use diesel::prelude::*;
 
 #[derive(Queryable, Serialize, Insertable)]
 pub struct Tag {
@@ -29,6 +30,16 @@ impl Tag {
 
         diesel::insert_into(tags::table).values(&tag).get_result(conn)
     }
+
+    fn update(id: Uuid, update_tag: UpdateTag, user: User, conn: &PgConnection) -> QueryResult<Self> {
+        diesel::update(
+            tags::table
+                .filter(tags::columns::id.eq(id))
+                .filter(tags::owner_id.eq(user.id)),
+        )
+        .set(update_tag)
+        .get_result(conn)
+    }
 }
 
 #[derive(Deserialize)]
@@ -37,13 +48,22 @@ pub struct NewTag {
     color: String,
 }
 
-mod routes {
-    use actix_web::{post, web, Error, HttpRequest, HttpResponse};
+#[derive(AsChangeset, Deserialize)]
+#[table_name = "tags"]
+pub struct UpdateTag {
+    name: String,
+    color: String,
+}
 
-    use super::{NewTag, Tag};
+mod routes {
+    use actix_web::{post, patch, web, Error, HttpRequest, HttpResponse};
+
+    use super::{NewTag, Tag, UpdateTag};
     use crate::database::exec_on_pool;
     use crate::utils::responsable::Responsable;
     use crate::DbPool;
+    use uuid::Uuid;
+
     #[post("/tags")]
     pub async fn create_tag(
         pool: web::Data<DbPool>,
@@ -56,4 +76,24 @@ mod routes {
             .await
             .into_response()
     }
+
+    #[patch("/tags/{id}")]
+    pub async fn update_tag(
+        pool: web::Data<DbPool>,
+        req: HttpRequest,
+        id: web::Path<Uuid>,
+        form: web::Json<UpdateTag>,
+    ) -> Result<HttpResponse, Error> {
+        let user = req.extensions().get().cloned().unwrap();
+
+        exec_on_pool(&pool, move |conn| Tag::update(id.into_inner(), form.into_inner(), user, conn))
+            .await
+            .into_response()
+    }
+
+    /*
+        DIT DOE JE NIET: #[get(/tags/{item_id}/)] -> Vec<Tag>
+
+        DIT IS BETER: #[get(/items/{id}/tags)] -> Vec<Tag>
+    */
 }
