eaade1dccf956c7cc29809271db9d25eda589ae9
DutchGhost <kasper199914@gmail.com>
2020-06-09 11:16:45 +0200

    make deletes wurk

--- src/tags/tags.rs
+++ src/tags/tags.rs
@@ -3,6 +3,8 @@ use crate::users::user::User;
 use diesel::pg::PgConnection;
 use diesel::QueryResult;
 use diesel::RunQueryDsl;
+use diesel::QueryDsl;
+use diesel::ExpressionMethods;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use diesel::prelude::*;
@@ -20,7 +22,7 @@ impl Tag {
         let NewTag { name, color } = new_tag;
         Self { id: Uuid::new_v4(), name, color, owner_id: user.id }
     }
-
+    
     fn create(
         new_tag: NewTag,
         user: User,
@@ -40,6 +42,20 @@ impl Tag {
         .set(update_tag)
         .get_result(conn)
     }
+
+    fn delete(
+        id: Uuid,
+        user: User,
+        connection: &PgConnection,
+    ) -> QueryResult<()> {
+        diesel::delete(
+            tags::table
+                .filter(tags::owner_id.eq(user.id))
+                .filter(tags::id.eq(id))
+        )
+        .get_result::<Tag>(connection)
+        .map(drop)
+    }
 }
 
 #[derive(Deserialize)]
@@ -56,7 +72,7 @@ pub struct UpdateTag {
 }
 
 mod routes {
-    use actix_web::{post, patch, web, Error, HttpRequest, HttpResponse};
+    use actix_web::{post, delete, patch, web, Error, HttpRequest, HttpResponse};
 
     use super::{NewTag, Tag, UpdateTag};
     use crate::database::exec_on_pool;
@@ -87,6 +103,18 @@ mod routes {
         let user = req.extensions().get().cloned().unwrap();
 
         exec_on_pool(&pool, move |conn| Tag::update(id.into_inner(), form.into_inner(), user, conn))
+            .await
+            .into_response()
+    }
+    
+    #[delete("/tags/{id}")]
+    pub async fn delete_tag(
+        pool: web::Data<DbPool>,
+        request: HttpRequest,
+        id: web::Path<Uuid>,
+    ) -> Result<HttpResponse, Error> {
+        let user = request.extensions().get().cloned().unwrap();
+        exec_on_pool(&pool, |conn| Tag::delete(id.into_inner(), user, &conn))
             .await
             .into_response()
     }
